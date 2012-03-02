dac189444cca4cd6bc55e20e7b1ff4b45ac37a1e
unknown <Joel@Cactus.(none)>
2012-03-01 22:11:52 -0500

    very rough, but sort photos based on EXIF data into directory structure year/month/year-month-day.  Also sorts video and raw photo files based on modified date.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SortPhotos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSourceDir = new System.Windows.Forms.TextBox();
+            this.txtDestPhotos = new System.Windows.Forms.TextBox();
+            this.btnSortNow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDestPhotosRaw = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDestinationVideos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkMakeCopies = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtSourceDir
+            // 
+            this.txtSourceDir.Location = new System.Drawing.Point(12, 31);
+            this.txtSourceDir.Name = "txtSourceDir";
+            this.txtSourceDir.Size = new System.Drawing.Size(339, 20);
+            this.txtSourceDir.TabIndex = 1;
+            // 
+            // txtDestPhotos
+            // 
+            this.txtDestPhotos.Location = new System.Drawing.Point(12, 74);
+            this.txtDestPhotos.Name = "txtDestPhotos";
+            this.txtDestPhotos.Size = new System.Drawing.Size(339, 20);
+            this.txtDestPhotos.TabIndex = 3;
+            // 
+            // btnSortNow
+            // 
+            this.btnSortNow.Location = new System.Drawing.Point(12, 216);
+            this.btnSortNow.Name = "btnSortNow";
+            this.btnSortNow.Size = new System.Drawing.Size(75, 23);
+            this.btnSortNow.TabIndex = 9;
+            this.btnSortNow.Text = "Sort now!";
+            this.btnSortNow.UseVisualStyleBackColor = true;
+            this.btnSortNow.Click += new System.EventHandler(this.btnSortNow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination Photos:";
+            // 
+            // txtDestPhotosRaw
+            // 
+            this.txtDestPhotosRaw.Location = new System.Drawing.Point(12, 115);
+            this.txtDestPhotosRaw.Name = "txtDestPhotosRaw";
+            this.txtDestPhotosRaw.Size = new System.Drawing.Size(339, 20);
+            this.txtDestPhotosRaw.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Destination Photos (Raw):";
+            // 
+            // txtDestinationVideos
+            // 
+            this.txtDestinationVideos.Location = new System.Drawing.Point(12, 155);
+            this.txtDestinationVideos.Name = "txtDestinationVideos";
+            this.txtDestinationVideos.Size = new System.Drawing.Size(339, 20);
+            this.txtDestinationVideos.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Destination Videos:";
+            // 
+            // chkMakeCopies
+            // 
+            this.chkMakeCopies.AutoSize = true;
+            this.chkMakeCopies.Location = new System.Drawing.Point(14, 182);
+            this.chkMakeCopies.Name = "chkMakeCopies";
+            this.chkMakeCopies.Size = new System.Drawing.Size(120, 17);
+            this.chkMakeCopies.TabIndex = 8;
+            this.chkMakeCopies.Text = "Make copies of files";
+            this.chkMakeCopies.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 262);
+            this.Controls.Add(this.chkMakeCopies);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSortNow);
+            this.Controls.Add(this.txtDestinationVideos);
+            this.Controls.Add(this.txtDestPhotosRaw);
+            this.Controls.Add(this.txtDestPhotos);
+            this.Controls.Add(this.txtSourceDir);
+            this.Name = "Form1";
+            this.Text = "Sort Photos and Videos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSourceDir;
+        private System.Windows.Forms.TextBox txtDestPhotos;
+        private System.Windows.Forms.Button btnSortNow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDestPhotosRaw;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDestinationVideos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkMakeCopies;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace SortPhotos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSortNow_Click(object sender, EventArgs e)
+        {
+            string sourceDir = txtSourceDir.Text;
+            string destDirPhotos = txtDestPhotos.Text;
+            string destDirPhotosRaw = txtDestPhotosRaw.Text;
+            string destDirVideos = txtDestinationVideos.Text;
+
+            using (StreamWriter successLog = new StreamWriter(Path.Combine(destDirPhotos, "SortPhotos-Success.log")))
+            using (StreamWriter errorLog = new StreamWriter(Path.Combine(destDirPhotos, "SortPhotos-Error.log")))
+            {
+                successLog.AutoFlush = true;
+                errorLog.AutoFlush = true;
+
+                SortDirectory(chkMakeCopies.Checked, sourceDir, destDirPhotos, destDirPhotosRaw, destDirVideos, successLog, errorLog);
+            }
+        }
+
+        private void SortDirectory(bool makeCopies, string sourceDir, string destDirPhotos, string destDirPhotosRaw, string destDirVideos, StreamWriter successLog, StreamWriter errorLog)
+        {
+            string[] fileFind = Directory.GetFiles(sourceDir);
+
+            foreach (string name in fileFind)
+            {
+                if (name == "." || name == "..")
+                    continue;
+
+                string path = name;
+                string destPath = "";
+                try
+                {
+                    string year, month, day;
+
+                    // try to get exif data
+                    try
+                    {
+                        Bitmap bitmap = new Bitmap(path);
+                        PropertyItem prop = bitmap.GetPropertyItem(36867);
+
+                        year = string.Format("{0}{1}{2}{3}", (char)prop.Value[0], (char)prop.Value[1], (char)prop.Value[2], (char)prop.Value[3]);
+                        month = string.Format("{0}{1}", (char)prop.Value[5], (char)prop.Value[6]);
+                        day = string.Format("{0}{1}", (char)prop.Value[8], (char)prop.Value[9]);
+                    }
+                    catch
+                    {
+                        // just use file created date
+                        DateTime date = File.GetLastWriteTime(path);
+                        year = date.Year.ToString("0000");
+                        month = date.Month.ToString("00");
+                        day = date.Day.ToString("00");
+                    }
+
+                    // copy/move to folder
+                    string destDir;
+                    switch (Path.GetExtension(name).ToLower())
+                    {
+                        case ".crw":
+                        case ".raw":
+                        case ".cr2":
+                        case ".dng":
+                            destDir = destDirPhotosRaw;
+                            break;
+                        case ".mov":
+                        case ".avi":
+                            destDir = destDirVideos;
+                            break;
+
+                        case ".jpg":
+                        case ".jpeg":
+                        case ".png":
+                        case ".bmp":
+                        default:
+                            destDir = destDirPhotos;
+                            break;
+                    }
+                    string destRelPath = string.Format("{0}\\{1}\\{2}-{3}-{4}", year, month, year, month, day);
+                    string thisDestDir = Path.Combine(destDir, destRelPath);
+                    destPath = Path.Combine(thisDestDir, Path.GetFileName(name));
+
+                    if (!File.Exists(destPath))
+                    {
+                        if (!Directory.Exists(thisDestDir))
+                            Directory.CreateDirectory(thisDestDir);
+
+                        if (makeCopies)
+                        {
+                            File.Copy(path, destPath);
+                            successLog.WriteLine("Copied '" + path + "' to '" + destPath + "'");
+                        }
+                        else
+                        {
+                            File.Move(path, destPath);
+                            successLog.WriteLine("Move '" + path + "' to '" + destPath + "'");
+                        }
+                    }
+                    else
+                    {
+                        successLog.WriteLine("File already exists '" + path + "' to '" + destPath + "'");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    successLog.WriteLine("Failing copying '" + path + "' to '" + destPath + "': " + ex.Message);
+                }
+            }
+
+            // directories
+            string[] dirs = Directory.GetDirectories(sourceDir);
+            foreach (string name in dirs)
+            {
+                if (name == "." || name == "..")
+                    continue;
+
+                string path = name;
+                SortDirectory(makeCopies, path, destDirPhotos, destDirPhotosRaw, destDirVideos, successLog, errorLog);
+
+            }
+        }
+    }
+}
